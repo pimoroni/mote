4fd5f2c9c442a6c3bff50a558c5a2177b6676d98
Phil Howard <Phil@gadgetoid.com>
2016-07-29 12:26:23 +0100

    Modified to use get_pixel_count

--- examples/test.py
+++ examples/test.py
@@ -22,7 +22,7 @@ try:
     # Display solid colour test
     for step in range(4):
         for channel in range(4):
-            for pixel in range(num_pixels):
+            for pixel in range(mote.get_pixel_count(channel + 1)):
                 r, g, b = colors[channel]
                 mote.set_pixel(channel + 1, pixel, r, g, b)
                 mote.show()
@@ -33,7 +33,7 @@ try:
     # Fade out pixels from their last state
     for step in range(170):
         for channel in range(4):
-            for pixel in range(num_pixels):
+            for pixel in range(mote.get_pixel_count(channel + 1)):
                 r, g, b = [int(c * 0.99) for c in mote.get_pixel(channel + 1, pixel)]
                 mote.set_pixel(channel + 1, pixel, r, g, b)
 
@@ -44,7 +44,7 @@ try:
     brightness = 0
     for h in range(1000):
         for channel in range(4):
-            for pixel in range(num_pixels):
+            for pixel in range(mote.get_pixel_count(channel + 1)):
                 hue = (h + (channel * num_pixels * 4) + (pixel * 4)) % 360
                 r, g, b = [int(c * brightness) for c in hsv_to_rgb(hue/360.0, 1.0, 1.0)]
                 mote.set_pixel(channel + 1, pixel, r, g, b)
