a1b74aee194a81e421e6de6038c5ccb4e5c58820
Phil Howard <phil@gadgetoid.com>
2016-08-02 12:47:43 +0100

    Cheerlights example

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 ![Mote](mote-logo.png)
 
 Buy the Mote controller & accessories here: https://shop.pimoroni.com/products/mote
+
+##Installation
+
+###Windows/Linux/Mac
+
+Install the mote library with pip, like so:
+
+```
+pip install mote
+```
+
+###Raspberry Pi (Raspbian)
+
+We've created a super-easy installation script that will install all pre-requisites and get your Mote up and running in a jiffy. To run it fire up Terminal which you'll find in Menu -> Accessories -> Terminal on your Raspberry Pi desktop like so:
+
+![Finding the terminal](terminal.jpg)
+
+In the new terminal window type:
+
+```bash
+curl -sS get.pimoroni.com/mote | bash
+```
+
+If you choose to download examples you'll find them in `/home/pi/Pimoroni/mote/examples`.
+
---
+++ python/examples/cheerlights.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import time
+import requests
+from colorsys import hsv_to_rgb
+from mote import Mote
+
+
+mote = Mote()
+
+num_pixels = 16
+
+mote.configure_channel(1, num_pixels, False)
+mote.configure_channel(2, num_pixels, False)
+mote.configure_channel(3, num_pixels, False)
+mote.configure_channel(4, num_pixels, False)
+
+
+try:
+    while True:
+        r = requests.get('http://api.thingspeak.com/channels/1417/feed.json')
+        j = r.json()
+        f = j['feeds'][-8:]
+
+        f = [element for index, element in enumerate(f) if index%2==0]
+
+        print(f)
+
+        channel = 1
+        for col in f:
+            col = col['field2']
+            r, g, b = tuple(ord(c) for c in col[1:].lower().decode('hex'))
+            for pixel in range(mote.get_pixel_count(channel)):
+                mote.set_pixel(channel, pixel, r, g, b)
+            channel += 1        
+
+        mote.show()
+
+        time.sleep(5)
+
+except KeyboardInterrupt:
+    mote.clear()
+    mote.show()
+    time.sleep(0.1)
